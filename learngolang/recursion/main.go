c5974efa1995b21bdfdc045a2c3adb4cf15c599c
Nikita <nikitafrolovbnc@gmail.com>
2021-09-18 23:40:12 +0300

    learn recursion and exception

---
+++ learngolang/recursion/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+func reportPanic() {
+	p := recover()
+	if p == nil {
+		return
+	}
+	err, ok := p.(error)
+	if ok {
+		fmt.Println(err)
+	} else {
+		panic(p)
+	}
+}
+
+func scanDirectory(path string) {
+	fmt.Println(path)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
+
+		if file.IsDir() {
+			scanDirectory(filePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			fmt.Println(filePath)
+		}
+	}
+}
+func main() {
+	defer reportPanic()
+	scanDirectory("my_dir")
+
+}
